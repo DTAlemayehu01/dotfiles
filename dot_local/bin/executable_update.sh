b2575111d66b78924c6de5cf2854a76694919c28
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2026-06-16 18:28:17 -0600

    new dotfile updates

--- dot_local/bin/executable_update.sh
+++ dot_local/bin/executable_update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+chezmoi update
 chezmoi -R apply
+chezmoi merge-all
 yay -Syu
 doom sync
 doom upgrade
 rustup update
 hyprpm update
 flatpak update
+
+# Better for first time setup?
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula" 
